c5c7bcc9e259b721db92753175c33519c2f46273
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-10-23 09:42:24 +0200

    Move 'RemoteFeedLoader' components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Flavio Serrazes on 23.10.20.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
