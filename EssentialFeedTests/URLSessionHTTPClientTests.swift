938cf04b42c7b2fa32c14d4900c250ee2d7ee94e
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-10-26 11:01:02 +0100

    Creates URLSession data task with URL on 'URLSessionHTTPClient.get(from URL)'

---
+++ EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,43 @@
+//
+//  Created by Flavio Serrazes on 26.10.20.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) {_, _, _ in }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_get_from_url_creates_data_task_with_url() {
+        let url = URL(string: "http://given-url.com")!
+        let session  = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+
+    //MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
