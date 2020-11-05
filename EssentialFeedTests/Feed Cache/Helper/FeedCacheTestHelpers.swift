a3c6a0286d488ea6fa5824663a38d29bdb6a8c68
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-11-05 15:42:47 +0100

    Move feed cache max age (7 days) to a computed var to clarify intent in code

--- EssentialFeedTests/Feed Cache/Helper/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helper/FeedCacheTestHelpers.swift
@@ -16,8 +16,10 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 }
 
 extension Date {
+    private var feedCacheMaxAgeInDays: Int { 7 }
+    
     func minusFeedCacheMaxAge() -> Date {
-        return adding(days: -7)
+        return adding(days: -feedCacheMaxAgeInDays)
     }
     
     func adding(days: Int) -> Date {
