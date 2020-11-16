a7740abef33607515764336a4ac787643049b8e4
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-11-16 10:31:53 +0100

    Load persistent container stores upon `CoreDataFeedStore` initialization

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -6,7 +6,12 @@ import Foundation
 import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
-    public init() {}
+    
+    private let container: NSPersistentContainer
+    
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
@@ -19,17 +24,46 @@ public final class CoreDataFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
     }
-    
-    private class ManagedCache: NSManagedObject {
-         @NSManaged var timestamp: Date
-         @NSManaged var feed: NSOrderedSet
+}
+
+private extension NSPersistentContainer {
+     enum LoadingError: Swift.Error {
+         case modelNotFound
+         case failedToLoadPersistentStores(Swift.Error)
      }
 
-     private class ManagedFeedImage: NSManagedObject {
-         @NSManaged var id: UUID
-         @NSManaged var imageDescription: String?
-         @NSManaged var location: String?
-         @NSManaged var url: URL
-         @NSManaged var cache: ManagedCache
+     static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+             throw LoadingError.modelNotFound
+         }
+
+         let container = NSPersistentContainer(name: name, managedObjectModel: model)
+         var loadError: Swift.Error?
+         container.loadPersistentStores { loadError = $1 }
+         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+
+         return container
      }
+ }
+
+ private extension NSManagedObjectModel {
+     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+         return bundle
+             .url(forResource: name, withExtension: "momd")
+             .flatMap { NSManagedObjectModel(contentsOf: $0) }
+     }
+ }
+    
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
 }
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
+
