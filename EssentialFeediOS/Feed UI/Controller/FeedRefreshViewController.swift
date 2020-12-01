3f363ca72ff9d389ff391e7977d813637c33f464
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-01 22:17:44 +0100

    Extract FeedRefreshViewController to its own file

---
+++ EssentialFeediOS/Feed UI/Controller/FeedRefreshViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Flavio Serrazes on 01.12.20.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedRefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
