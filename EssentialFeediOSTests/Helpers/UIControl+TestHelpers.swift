c8b04b14d9b6e09a9112ab505c7fe597e244f84e
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-01 15:01:36 +0100

    Extract duplicate logic into a reusable UIControl extension

---
+++ EssentialFeediOSTests/Helpers/UIControl+TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Flavio Serrazes on 01.12.20.
+//
+
+import UIKit
+
+extension UIControl {
+     func simulate(event: UIControl.Event) {
+         allTargets.forEach { target in
+             actions(forTarget: target, forControlEvent: event)?.forEach {
+                 (target as NSObject).perform(Selector($0))
+             }
+         }
+     }
+ }
