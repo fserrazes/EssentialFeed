15f855a5396540f52f85ef9d5f5527b13f2869b9
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-12 21:03:22 +0100

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presenter/FeedErrorViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Flavio Serrazes on 12.12.20.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
