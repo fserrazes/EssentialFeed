5cd5a3f4f3749a473d4527b79de6296ab35d0d65
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-15 21:21:28 +0100

    Add `HTTPClientTask` so clients can cancel HTTP requests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -32,6 +32,25 @@ class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_cancelGetFromURLTask_cancelsURLRequest() {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for request")
+        
+        let task = makeSUT().get(from: url) { result in
+            switch result {
+                case let .failure(error as NSError) where error.code == URLError.cancelled.rawValue:
+                    break
+                    
+                default:
+                    XCTFail("Expected cancelled result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        task.cancel()
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     func test_get_from_url_fails_on_requests_error() {
         let requestError = anyNSError()
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
