9897dfe164880a7ba98aa9edc0acfd023c004a1b
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-16 20:38:26 +0100

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Flavio Serrazes on 16.12.20.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+ }
