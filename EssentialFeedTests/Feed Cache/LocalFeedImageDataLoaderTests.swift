412879bf8258af45de0504646ca06a7254dd70c7
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-16 14:35:11 +0100

    `LocalFeedImageDataLoader.loadImageData` delivers stored data when store finds image data for url

--- EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -31,7 +31,7 @@ final class LocalFeedImageDataLoader {
         store.retrieve(dataForURL: url) { result in
             completion(result
                         .mapError { _ in Error.failed }
-                        .flatMap { _ in .failure(Error.notFound) })
+                        .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound) })
         }
         return Task()
     }
@@ -71,8 +71,13 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         })
     }
     
-    private func notFound() -> FeedImageDataLoader.Result {
-        return .failure(LocalFeedImageDataLoader.Error.notFound)
+    func test_loadImageDataFromURL_deliversStoredDataOnFoundData() {
+        let (sut, store) = makeSUT()
+        let foundData = anyData()
+        
+        expect(sut, toCompleteWith: .success(foundData), when: {
+            store.complete(with: foundData)
+        })
     }
     
     // MARK: - Helpers
@@ -87,6 +92,10 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     
     private func failed() -> FeedImageDataLoader.Result {
         return .failure(LocalFeedImageDataLoader.Error.failed)
+    }
+    
+    private func notFound() -> FeedImageDataLoader.Result {
+        return .failure(LocalFeedImageDataLoader.Error.notFound)
     }
     
     private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
