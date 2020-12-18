256a7627c5ac385e2e2bf6c745476f9040c9427b
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-18 21:50:05 +0100

    Move `FeedLoaderWithFallbackComposite` to production

---
+++ EssentialApp/EssentialApp/FeedLoaderWithFallbackComposite.swift
@@ -0,0 +1,27 @@
+//
+//  Created by Flavio Serrazes on 18.12.20.
+//
+
+import EssentialFeed
+
+public class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+    
+    public init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+                case .success:
+                    completion(result)
+                    
+                case .failure:
+                    self?.fallback.load(completion: completion)
+            }
+        }
+    }
+}
--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -4,28 +4,8 @@
 
 import XCTest
 import EssentialFeed
+import EssentialApp
 
-class FeedLoaderWithFallbackComposite: FeedLoader {
-    private let primary: FeedLoader
-    private let fallback: FeedLoader
-    
-    init(primary: FeedLoader, fallback: FeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-                case .success:
-                    completion(result)
-                    
-                case .failure:
-                    self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
 class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
