7320acf2c096e29806957bfbdd1848f8e26f8066
Flavio Serrazes <flavio.serrazes@icloud.com>
2020-12-19 21:11:13 +0100

    Move `FeedCache` to the `Feed Feature` production group

---
+++ EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Flavio Serrazes on 19.12.20.
+//
+
+import Foundation
+
+public protocol FeedCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
