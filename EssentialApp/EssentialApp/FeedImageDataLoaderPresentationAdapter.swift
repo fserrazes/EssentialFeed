b39750d629fb554a0b758da75e7f99f38f827647
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-04 00:11:49 +0100

    Replace FeedImageDataLoader composition with Combine operators

--- EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
@@ -1,38 +1,43 @@
 //
 //  Created by Flavio Serrazes on 08.12.20.
 //
-
+import Combine
+import Foundation
 import EssentialFeed
 import EssentialFeediOS
 
 final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-     private let model: FeedImage
-     private let imageLoader: FeedImageDataLoader
-     private var task: FeedImageDataLoaderTask?
-
-     var presenter: FeedImagePresenter<View, Image>?
-
-     init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-         self.model = model
-         self.imageLoader = imageLoader
-     }
-
-     func didRequestImage() {
-         presenter?.didStartLoadingImageData(for: model)
-
-         let model = self.model
-         task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-             switch result {
-             case let .success(data):
-                 self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-
-             case let .failure(error):
-                 self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-             }
-         }
-     }
-
-     func didCancelImageRequest() {
-         task?.cancel()
-     }
- }
+    private let model: FeedImage
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private var cancellable: Cancellable?
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
+    init(model: FeedImage, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        let model = self.model
+        
+        cancellable = imageLoader(model.url).sink(
+            receiveCompletion: { [weak self] completion in
+                switch completion {
+                    case .finished: break
+                    
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+                }
+                
+            }, receiveValue: { [weak self] data in
+                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+            })
+    }
+    
+    func didCancelImageRequest() {
+        cancellable?.cancel()
+    }
+}
