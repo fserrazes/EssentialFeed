038c47a298f8661ca8038e83caca86f197b84848
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-24 22:51:21 +0100

    Add ImageComment data model

---
+++ EssentialFeed/Feed API/ImageComment.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Flavio Serrazes on 22.10.20.
+//
+
+import Foundation
+
+public struct ImageComment: Hashable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
