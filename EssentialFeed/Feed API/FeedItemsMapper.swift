e90aaedf3944fd834335a1e0372a020362f7b203
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-26 21:58:44 +0100

    Test FeedItemsMapper in isolation

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-final class FeedItemsMapper {
+public final class FeedItemsMapper {
     private struct Root: Decodable {
         private let items: [Item]
         
@@ -20,7 +20,7 @@ final class FeedItemsMapper {
         }
     }
     
-    static func map (_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+    public static func map (_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
         guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteFeedLoader.Error.invalidData
         }
