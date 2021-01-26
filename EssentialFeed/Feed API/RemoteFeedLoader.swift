d1cdec97979d89234fb1ffca2731aab6ec1d8416
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-26 21:19:38 +0100

    Replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -4,41 +4,11 @@
 
 import Foundation
 
-public final class RemoteFeedLoader: FeedLoader {
-    private let url: URL
-    private let client: HTTPClient
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
+
+public extension RemoteFeedLoader {
     
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = FeedLoader.Result
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result)-> Void) {
-        client.get(from: url) { [weak self] result  in
-            guard self != nil else { return }
-            switch result {
-                case let .success((data, response)):
-                    completion(RemoteFeedLoader.map(data, from: response))
-                    
-                case .failure:
-                    completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map( _ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemsMapper.map(data, response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
     }
 }
