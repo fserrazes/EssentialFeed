0aac2bfcbac82b6b931a9771c3b9531a3ccd6363
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-28 13:56:41 +0100

    Add FeedImagePresenter map

--- EssentialFeed/Feed Presenter/FeedImagePresenter.swift
+++ EssentialFeed/Feed Presenter/FeedImagePresenter.swift
@@ -34,4 +34,10 @@ public final class FeedImagePresenter<View: FeedImageView, Image> where View.Ima
         view.display(FeedImageViewModel(description: model.description, location: model.location,
                                         image: nil, isLoading: false, shouldRetry: true))
     }
+    
+    public static func map (_ image: FeedImage) -> FeedImageViewModel<Image> {
+        return FeedImageViewModel(description: image.description, location: image.location,
+                                  image: nil, isLoading: false, shouldRetry: false)
+        
+    }
 }
