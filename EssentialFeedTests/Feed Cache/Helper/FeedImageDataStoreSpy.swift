3725d4c400c58f55f38979b098fd0b96ec7fac04
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-02-17 21:06:18 +0100

    Save images synchronously

--- EssentialFeedTests/Feed Cache/Helper/FeedImageDataStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helper/FeedImageDataStoreSpy.swift
@@ -11,21 +11,21 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
         case retrieve(dataFor: URL)
     }
     
-    private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
+    private var insertionResult: Result<Void, Error>?
     private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
     private(set) var receivedMessages = [Message]()
     
-    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+    func insert(_ data: Data, for url: URL) throws {
         receivedMessages.append(.insert(data: data, for: url))
-        insertionCompletions.append(completion)
+        try insertionResult?.get()
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
-        insertionCompletions[index](.failure(error))
+        insertionResult = .failure(error)
     }
     
     func completeInsertionSuccessfully(at index: Int = 0) {
-        insertionCompletions[index](.success(()))
+        insertionResult = .success(())
     }
     
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
