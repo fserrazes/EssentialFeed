04e471fa51abb008c40180ba9b40c93ad0280cb6
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-02-19 00:09:50 +0100

    Make FeedImageDataLoader sync

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -20,48 +20,23 @@ extension LocalFeedImageDataLoader: FeedImageDataCache {
     public func save(_ data: Data, for url: URL) throws {
         do {
             try store.insert(data, for: url)
-        } catch {
-            throw SaveError.failed
-        }
+        } catch { throw SaveError.failed }
     }
 }
 
 extension LocalFeedImageDataLoader: FeedImageDataLoader {
-    public typealias LoadResult = FeedImageDataLoader.Result
-    
     public enum LoadError: Swift.Error {
         case failed
         case notFound
     }
 
-    private final class LoadImageDataTask: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-
-        func cancel() {
-            preventFurtherCompletions()
-        }
-
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-
-    public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
-        let task = LoadImageDataTask(completion)
-        task.complete(with: Swift.Result {
-            try store.retrieve(dataForURL: url)
-        }.mapError { _ in LoadError.failed }
-        .flatMap { data in
-            data.map { .success($0) } ?? .failure(LoadError.notFound)
-        })
-        return task
+    public func loadImageData(from url: URL) throws -> Data {
+        do {
+            if let data =  try store.retrieve(dataForURL: url) {
+                return data
+            }
+        } catch { throw LoadError.failed }
+        
+        throw LoadError.notFound
     }
 }
