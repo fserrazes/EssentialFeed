c71331798b29346f4661e4caa5fb5b7216b8e0c3
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-02-21 22:38:44 +0100

    Make FeedCache sync

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -125,6 +125,14 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         return sut
     }
     
+    private func save(_ feed:[FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+        do {
+            try loader.save(feed)
+        } catch {
+            XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
+        }
+    }
+
     private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
         loader.validateCache() { result in
@@ -149,18 +157,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
-    
-    private func save(_ feed:[FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
-        let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { result in
-            if case let Result.failure(error) = result {
-                XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
-            }
-            saveExp.fulfill()
-        }
-        wait(for: [saveExp], timeout: 1.0)
-    }
-    
+        
     private func save(_ data: Data, for url: URL, with loader: LocalFeedImageDataLoader, file: StaticString = #file, line: UInt = #line) {
         do {
             try loader.save(data, for: url)
