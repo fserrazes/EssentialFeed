31d360d2442d4ce09b53d060eabe53ea697d93c0
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-09-21 16:29:49 +0100

    Move EssentialFeediOS folder to new folder

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Flavio Serrazes on 31.01.21.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
