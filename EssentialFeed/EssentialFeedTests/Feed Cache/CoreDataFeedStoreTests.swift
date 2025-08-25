93898022029f717e472c28f45e467d2c90161ef9
Flavio Serrazes <flavio.serrazes@icloud.com>
2025-08-25 21:10:06 +0200

    Rename FeedStoreSpecs to be consistent

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,67 +6,67 @@ import XCTest
 import EssentialFeed
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
-    func test_retrieve_delivers_empty_on_empty_cache() throws {
+    func test_retrieve_deliversEmptyOnEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
         }
     }
     
-    func test_retrieve_has_no_side_effects_on_empty_cache() throws {
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
         }
     }
     
-    func test_retrieve_delivers_found_values_on_non_empty_cache() throws {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
         }
     }
     
-    func test_retrieve_has_no_side_effects_on_non_empty_cache() throws {
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
         }
     }
     
-    func test_insert_delivers_no_error_on_empty_cache() throws {
+    func test_insert_deliversNoErrorOnEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
         }
     }
     
-    func test_insert_delivers_no_error_on_non_empty_cache() throws {
+    func test_insert_deliversNoErrorOnNonEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
         }
     }
     
-    func test_insert_overrides_previously_inserted_cache_values() throws {
+    func test_insert_overridesPreviouslyInsertedCacheValues() throws {
         try makeSUT() { sut in
             self.assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
         }
     }
     
-    func test_delete_delivers_no_error_on_empty_cache() throws {
+    func test_delete_deliversNoErrorOnEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
         }
     }
     
-    func test_delete_has_no_side_effects_on_empty_cache() throws {
+    func test_delete_hasNoSideEffectsOnEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
         }
     }
     
-    func test_delete_delivers_no_error_on_non_empty_cache() throws {
+    func test_delete_deliversNoErrorOnNonEmptyCache() throws {
         try makeSUT() { sut in
             self.assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
         }
     }
     
-    func test_delete_empties_previously_inserted_cache() throws {
+    func test_delete_emptiesPreviouslyInsertedCache() throws {
         try makeSUT() { sut in
             self.assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
         }
--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -5,19 +5,19 @@
 import Foundation
 
 protocol FeedStoreSpecs {
-    func test_retrieve_delivers_empty_on_empty_cache() throws
-    func test_retrieve_has_no_side_effects_on_empty_cache() throws
-    func test_retrieve_delivers_found_values_on_non_empty_cache() throws
-    func test_retrieve_has_no_side_effects_on_non_empty_cache() throws
+    func test_retrieve_deliversEmptyOnEmptyCache() throws
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() throws
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws
 
-    func test_insert_delivers_no_error_on_empty_cache() throws
-    func test_insert_delivers_no_error_on_non_empty_cache() throws
-    func test_insert_overrides_previously_inserted_cache_values() throws
+    func test_insert_deliversNoErrorOnEmptyCache() throws
+    func test_insert_deliversNoErrorOnNonEmptyCache() throws
+    func test_insert_overridesPreviouslyInsertedCacheValues() throws
 
-    func test_delete_delivers_no_error_on_empty_cache() throws
-    func test_delete_has_no_side_effects_on_empty_cache() throws
-    func test_delete_delivers_no_error_on_non_empty_cache() throws
-    func test_delete_empties_previously_inserted_cache() throws
+    func test_delete_deliversNoErrorOnEmptyCache() throws
+    func test_delete_hasNoSideEffectsOnEmptyCache() throws
+    func test_delete_deliversNoErrorOnNonEmptyCache() throws
+    func test_delete_emptiesPreviouslyInsertedCache() throws
 }
 
 protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
