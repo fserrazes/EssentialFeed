96ecb045cbc82cd1e376b3235d461d8d0e203e53
Flavio Serrazes <flavio.serrazes@icloud.com>
2025-12-17 16:06:09 +0100

    Migrate CoreDataFeedStoreTests to new async API

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -5,34 +5,34 @@
 import Foundation
 
 protocol FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache() throws
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() throws
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws
+    func test_retrieve_deliversEmptyOnEmptyCache() async throws
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() async throws
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() async throws
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() async throws
 
-    func test_insert_deliversNoErrorOnEmptyCache() throws
-    func test_insert_deliversNoErrorOnNonEmptyCache() throws
-    func test_insert_overridesPreviouslyInsertedCacheValues() throws
+    func test_insert_deliversNoErrorOnEmptyCache() async throws
+    func test_insert_deliversNoErrorOnNonEmptyCache() async throws
+    func test_insert_overridesPreviouslyInsertedCacheValues() async throws
 
-    func test_delete_deliversNoErrorOnEmptyCache() throws
-    func test_delete_hasNoSideEffectsOnEmptyCache() throws
-    func test_delete_deliversNoErrorOnNonEmptyCache() throws
-    func test_delete_emptiesPreviouslyInsertedCache() throws
+    func test_delete_deliversNoErrorOnEmptyCache() async throws
+    func test_delete_hasNoSideEffectsOnEmptyCache() async throws
+    func test_delete_deliversNoErrorOnNonEmptyCache() async throws
+    func test_delete_emptiesPreviouslyInsertedCache() async throws
 }
 
 protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
-    func test_retrieve_delivers_failure_on_retrieval_error() throws
-    func test_retrieve_has_no_side_effects_on_failure() throws
+    func test_retrieve_delivers_failure_on_retrieval_error() async throws
+    func test_retrieve_has_no_side_effects_on_failure() async throws
 }
 
 protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
-    func test_insert_delivers_error_on_insertion_error() throws
+    func test_insert_delivers_error_on_insertion_error() async throws
     func test_insert_has_no_side_effects_on_insertion_error() throws
 }
 
 protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
-    func test_delete_delivers_error_on_deletion_error() throws
-    func test_delete_has_no_side_effects_on_deletion_error() throws
+    func test_delete_delivers_error_on_deletion_error() async throws
+    func test_delete_has_no_side_effects_on_deletion_error() async throws
 }
 
 typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
