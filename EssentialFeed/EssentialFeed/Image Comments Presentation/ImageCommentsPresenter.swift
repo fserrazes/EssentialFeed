86d883b2cec415af5d4d48452dc2ad73964a9f6a
Flavio Serrazes <flavio.serrazes@icloud.com>
2025-12-18 13:13:57 +0100

    Conform ImageCommentsViewModel with Sendable

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -4,15 +4,38 @@
 
 import Foundation
 
+import Foundation
+
+public struct ImageCommentsViewModel: Sendable {
+    public let comments: [ImageCommentViewModel]
+}
+
+public struct ImageCommentViewModel: Hashable, Sendable {
+    public let message: String
+    public let date: String
+    public let username: String
+    
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
+
 public final class ImageCommentsPresenter {
     public static var title: String {
-        let bundle = Bundle(for: ImageCommentsPresenter.self)
-        return NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE", tableName: "ImageComments", bundle: bundle, comment: "Title for the iamge comments view")
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+                          tableName: "ImageComments",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Title for the image comments view")
     }
     
-    public static func map(_ comments: [ImageComment],
-            currentDate: Date = Date(), calendar: Calendar = .current, locale: Locale = .current) -> ImageCommentsViewModel {
-        
+    public static func map(
+        _ comments: [ImageComment],
+        currentDate: Date = Date(),
+        calendar: Calendar = .current,
+        locale: Locale = .current
+    ) -> ImageCommentsViewModel {
         let formatter = RelativeDateTimeFormatter()
         formatter.calendar = calendar
         formatter.locale = locale
@@ -23,21 +46,5 @@ public final class ImageCommentsPresenter {
                 date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
                 username: comment.username)
         })
-    }
-}
-
-public struct ImageCommentsViewModel: Sendable {
-    public let comments: [ImageCommentViewModel]
-}
-
-public struct ImageCommentViewModel: Hashable {
-    public let message: String
-    public let date: String
-    public let username: String
-
-    public init(message: String, date: String, username: String) {
-        self.message = message
-        self.date = date
-        self.username = username
     }
 }
