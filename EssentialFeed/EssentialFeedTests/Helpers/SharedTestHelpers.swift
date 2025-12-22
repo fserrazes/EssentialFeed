2c6d7652afa22ba1d8c83c41cc46f6104d44a956
Flavio Serrazes <flavio.serrazes@icloud.com>
2025-12-22 13:11:02 +0100

    Update SharedTestHelpers

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -3,7 +3,6 @@
 //
 
 import Foundation
-import EssentialFeed
 
 func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
@@ -17,23 +16,27 @@ func anyData() -> Data {
     return Data("any data".utf8)
 }
 
-func uniqueFeed() -> [FeedImage] {
-    return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
 }
 
-private class DummyView: ResourceView {
-    func display(_ viewModel: Any) {}
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
 }
 
-@MainActor
-var loadError: String {
-    LoadResourcePresenter<Any, DummyView>.loadError
-}
-
-var feedTitle: String {
-    FeedPresenter.title
-}
-
-var commentsTitle: String {
-    ImageCommentsPresenter.title
+extension Date {
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+    
+    func adding(minutes: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .day, value: days, to: self)!
+    }
 }
